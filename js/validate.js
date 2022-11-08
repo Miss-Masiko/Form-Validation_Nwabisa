5c6d13019ee340755a42adab42d18ac3517cf53a
Nwabisa Milisant Masiko <99478913+Miss-Masiko@users.noreply.github.com>
2022-11-08 10:17:52 +0200

    Adding task to the list
    Added JS Function
    Dom Api concept

---
+++ js/validate.js
@@ -0,0 +1,19 @@
+function newTask(){
+
+    // Dom api references
+
+    // 1. get the newTask element/inpu by its own id
+    // 2. create an if statement to prevent HTML Characters
+
+    if(true){
+        document.getElementById("error").style.display ="Block";
+    }
+
+    // 3. create a new li element using createElement dom api
+    // 4. create a textnode (save this to a variable)
+    // 5. append this variable to the <li> variable by using .appendChild
+    // 6. Append your new list to the HTML unordered list
+    
+}
+
+newTask();
